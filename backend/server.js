519ed4a34aec5328110019efa29ad4168366f68c
AshishManer <108808771+ash44444@users.noreply.github.com>
2025-07-12 09:54:40 +0530

    Update server.js
    five

--- backend/server.js
+++ backend/server.js
@@ -46,18 +46,10 @@ const __dirname1 = path.resolve();
 // --------------------------deployment------------------------------
 
 const port = process.env.PORT || 6000;
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
+ app.get("/", (req, res) => {
     res.send("API is running..");
   });
-}
+
 
 // --------------------------deployment------------------------------
 
